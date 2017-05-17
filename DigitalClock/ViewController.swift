0afcd4c32a0543eb21a53fe19e433f924a6c6011
StefanKollaart <garbera@icloud.com>
2017-05-17 14:01:36 +0200

    basic time show

--- DigitalClock/ViewController.swift
+++ DigitalClock/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateTimer() {
+        let timeFormatter = DateFormatter()
+        timeFormatter.timeStyle = .medium
+        label.text = timeFormatter.string(from: Date())
     }
 
 
